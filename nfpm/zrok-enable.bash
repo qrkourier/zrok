eab5737daaedb45c1d73e7802d50a2835dfd97ff
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-11-14 00:27:25 -0500

    require systemd to run the enable script

--- nfpm/zrok-enable.bash
+++ nfpm/zrok-enable.bash
@@ -7,17 +7,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-set -x
-
 BASENAME=$(basename "$0")
 DEFAULT_ZROK_ENVIRONMENT_NAME="zrok-share.service on $(hostname -s)"
 
 if (( $# )); then
   case $1 in
     -h|*help)
-      echo -e "Usage: ${BASENAME} [filename|-]\n"\
-            "\tFILENAME\tfile containing environment variables to set\n"\
-            "\t-\t\tread environment variable assignments from stdin\n"
+      echo -e \
+        "Usage: ${BASENAME} [filename|-]\n"\
+        "\tFILENAME\tfile containing environment variables to set\n"\
+        "\t-\t\tread environment variable assignments from stdin\n"
       exit 0
       ;;
   esac
@@ -27,60 +26,27 @@ fi
 if [[ -n "${STATE_DIRECTORY:-}" ]]; then
   export HOME="${STATE_DIRECTORY%:*}"
 else
-  # assume we're enabling for a system service, not a user service, if run outside systemd
-  export HOME="/var/lib/private/zrok-share"
-  if ! [[ -d "${HOME}" ]]; then
-    systemctl start --no-block zrok-share.service
-    until [[ -d "${HOME}" || ${TRIES:=1} -gt 2 ]]; do
-      sleep 1
-      (( TRIES++ ))
-    done
-    systemctl stop --no-block zrok-share.service
-    if [[ -d "${HOME}" ]]; then
-      UIDGID=$(stat -c "%u:%g" "${HOME}")
-    else
-      echo "ERROR: HOME=${HOME} is not a directory" >&2
-      exit 1
-    fi
-  fi
+  echo "ERROR: STATE_DIRECTORY is undefined. This script must be run from systemd because it runs as a"\
+    " dynamically-allocated user and exclusively manages the files in STATE_DIRECTORY" >&2
+  exit 1
 fi
 
 if [[ -s ~/.zrok/environment.json ]]; then
   echo "INFO: zrok environment is already enabled. Delete '$(realpath ~/.zrok/environment.json)' if you want to create a"\
         " new environment."
   exit 0
-else
-  echo "DEBUG: $(id) HOME=${HOME} $(stat ~/.zrok/)"
 fi
 
 if (( $# )); then
-  if [[ "$1" == "-" ]]; then
-    source <(cat)
-  elif [[ -s "$1" ]]; then
+  if [[ -s "$1" ]]; then
     source "$1"
   else
     echo "ERROR: \$1="$1" is empty or not a readable file" >&2
     exit 1
   fi
 else
-  if [[ -z "${ZROK_ENABLE_TOKEN:-}" ]]; then
-    read -sr -p "Enter zrok enable token: " ZROK_ENABLE_TOKEN
-    echo
-  fi
-  if [[ -z "${ZROK_ENABLE_TOKEN}" ]]; then
-    echo "ERROR: need ZROK_ENABLE_TOKEN or filename argument to read env configuration" >&2
-    exit 1
-  fi
-  if [[ -z "${ZROK_ENVIRONMENT_NAME:-}" ]]; then
-    read -r -p "Enter zrok environment name [${DEFAULT_ZROK_ENVIRONMENT_NAME}]: " ZROK_ENVIRONMENT_NAME
-    : "${ZROK_ENVIRONMENT_NAME:=${DEFAULT_ZROK_ENVIRONMENT_NAME}}"
-  fi
-  if [[ -z "${ZROK_API_ENDPOINT:-}" ]]; then
-    read -r -p "Enter zrok API endpoint [https://api.zrok.io]: " ZROK_API_ENDPOINT
-    if [[ -n "${ZROK_API_ENDPOINT}" ]]; then
-      export ZROK_API_ENDPOINT
-    fi
-  fi
+  echo "ERROR: need filename argument to read environment configuration" >&2
+  exit 1
 fi
 
 if [[ -z "${ZROK_ENABLE_TOKEN}" ]]; then
